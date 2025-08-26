352fb9a5e1c0518f19114ceac419ab57a2bb0e89
Akhil <rawatakhil5624@gmail.com>
2025-08-27 00:02:20 +0530

    frontend started

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -1,8 +1,9 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
-export async function POST(request: Request) {
+export async function POST() {
     const apiKey = process.env.GEMINI_API_KEY;
+    console.log(apiKey);
     if (!apiKey) {
         return NextResponse.json({ error: "GEMINI_API_KEY environment variable is not set." }, { status: 500 });
     }
@@ -10,16 +11,16 @@ export async function POST(request: Request) {
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
-    const body = await request.json();
-    const prompt = body.prompt || "what is javascript";
+    // const body = await request.json();
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separated by '||'. These questions are for an anonymous social messaging platform, like Qooh.me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: 'What's a hobby you've recently started?||If you could have dinner with any historical figure, who would it be?|| What's a simple thing that makes you happy?'. Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment."
 
     try {
         const result = await model.generateContent(prompt);
-        return NextResponse.json({ message: result.response.text });
+        return NextResponse.json({ message: result.response.text() });
     } catch (error: any) {
-         return Response.json({
+        return NextResponse.json({
             success: false,
-            message: "idk what the fuck bro"
-        }, { status: 500 })
+            message: "Something went wrong"
+        }, { status: 500 });
     }
 }
