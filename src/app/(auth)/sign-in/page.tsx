5ab724984033a49e50cfa57d9af70b1e95b7ff78
Akhil463 <akhil.rawat@jagrannewmedia.com>
2025-09-19 18:38:32 +0530

    iusdhfb

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,11 +4,13 @@ import { useDebounceValue } from "usehooks-ts";
 import { z } from "zod"
 import { toast } from "sonner"
 import { useRouter } from "next/navigation";
-import { useForm } from "react-hook-form";
+import { Form, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod"
 import { signupSchema } from "@/schemas/signupSchema";
 import axios, { Axios, AxiosError } from "axios"
 import { ApiResponse } from "@/types/apiResponse";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { Button } from "@react-email/components";
 const Page = () => {
   const [username, setUsername] = useState('');
   const [usernameMessage, setUsernameMessage] = useState('')
@@ -54,16 +56,38 @@ const Page = () => {
       setisFormSubmitting(false)
     } catch (error) {
       const axiosError = error as AxiosError<ApiResponse>;
-     let errorMessage  = axiosError.response?.data.message;
-     toast("Signup Failed ", {
-      description: errorMessage 
-     })
-     setisFormSubmitting(false);
+      let errorMessage = axiosError.response?.data.message;
+      toast("Signup Failed ", {
+        description: errorMessage
+      })
+      setisFormSubmitting(false);
     }
   }
   return (<>
-    <div>Page</div>
+
     {toast("Event has been created.")}
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>
+                Username
+              </FormLabel>
+              <FormControl>
+                <input placeholder="username" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is the Display name
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   </>
   )
 }
