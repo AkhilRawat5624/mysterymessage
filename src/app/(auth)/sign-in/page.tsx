c0e7494b19b1d846640235b11f7655b13b9b1495
Akhil <rawatakhil5624@gmail.com>
2025-09-21 22:36:27 +0530

    u[pdate

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react"
 import { useDebounceValue } from "usehooks-ts";
 import { z } from "zod"
+import Link from "next/link";
 import { toast } from "sonner"
 import { useRouter } from "next/navigation";
 import { Form, useForm } from "react-hook-form";
@@ -9,8 +10,9 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { signupSchema } from "@/schemas/signupSchema";
 import axios, { Axios, AxiosError } from "axios"
 import { ApiResponse } from "@/types/apiResponse";
-import { FormControl, FormDescription, FormField, FormItem, FormLabel } from "@/components/ui/form";
+import { FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Button } from "@react-email/components";
+import { Input } from "@/components/ui/input";
 const Page = () => {
   const [username, setUsername] = useState('');
   const [usernameMessage, setUsernameMessage] = useState('')
@@ -63,33 +65,54 @@ const Page = () => {
       setisFormSubmitting(false);
     }
   }
-  return (<>
-
-    {toast("Event has been created.")}
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>
-                Username
-              </FormLabel>
-              <FormControl>
-                <input placeholder="username" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is the Display name
-              </FormDescription>
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  </>
-  )
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <Input {...field} placeholder="username"/>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full' type="submit">Sign In</Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  
+  );
 }
 
 export default Page;
